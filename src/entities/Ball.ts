6b6515f0789eade3052837f9c85080214b21fd66
vlad20061308-png <vlad20061308@ukr.net>
2026-04-02 18:12:11 +0200

    Update Ball.ts

--- src/entities/Ball.ts
+++ src/entities/Ball.ts
@@ -1,5 +1,12 @@
 import type { Entity } from './Entity';
 
+export type BallRarity =
+  | 'common'
+  | 'rare'
+  | 'epic'
+  | 'legendary'
+  | 'mythical';
+
 export class Ball implements Entity {
   constructor(
     public id: string,
@@ -8,5 +15,6 @@ export class Ball implements Entity {
     public radius: number = 8,
     public velocityX: number = 0,
     public velocityY: number = 0,
+    public rarity: BallRarity = 'common',
   ) {}
 }
