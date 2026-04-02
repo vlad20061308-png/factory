be8f02759985cf56ef27eaa5f41d2795edadb010
vlad20061308-png <vlad20061308@ukr.net>
2026-04-02 16:13:50 +0200

    Update SpawnSystem.ts

--- src/systems/SpawnSystem.ts
+++ src/systems/SpawnSystem.ts
@@ -2,6 +2,6 @@ import { Ball } from '../entities/Ball';
 
 export class SpawnSystem {
   spawnBall(): Ball {
-    return new Ball(crypto.randomUUID(), 400, 300);
+    return new Ball(crypto.randomUUID(), 400, 300, 10, 180, 0);
   }
 }
