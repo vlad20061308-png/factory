28544b54569629ce168b701e27f52871f5eaf36e
vlad20061308-png <vlad20061308@ukr.net>
2026-04-02 18:24:11 +0200

    Update Game.ts

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,9 +1,14 @@
 import { CanvasRenderer } from '../render/CanvasRenderer';
 import { GameState } from '../state/GameState';
 import { MovementSystem } from '../systems/MovementSystem';
+import { SpawnSystem } from '../systems/SpawnSystem';
+
+const SPAWN_INTERVAL_MS = 2000;
 
 export class Game {
   private readonly movementSystem = new MovementSystem();
+  private readonly spawnSystem = new SpawnSystem();
+  private spawnTimer = 0;
 
   constructor(
     private readonly state: GameState,
@@ -12,9 +17,21 @@ export class Game {
 
   update(deltaTime: number): void {
     this.state.elapsedTime += deltaTime;
+    this.spawnTimer += deltaTime;
+
+    while (this.spawnTimer >= SPAWN_INTERVAL_MS) {
+      this.state.balls.push(this.spawnSystem.spawnBall());
+      this.spawnTimer -= SPAWN_INTERVAL_MS;
+    }
+
+    const canvasWidth = this.renderer.getCanvasWidth();
 
     for (const ball of this.state.balls) {
       this.movementSystem.updateBall(ball, deltaTime);
+
+      if (ball.x - ball.radius > canvasWidth) {
+        ball.x = -ball.radius;
+      }
     }
   }
 
