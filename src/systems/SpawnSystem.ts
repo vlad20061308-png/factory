ff96d451d9e23878aa871f0be542216a674ea3dd
vlad20061308-png <vlad20061308@ukr.net>
2026-04-02 18:14:47 +0200

    Update SpawnSystem.ts

--- src/systems/SpawnSystem.ts
+++ src/systems/SpawnSystem.ts
@@ -1,7 +1,18 @@
-import { Ball } from '../entities/Ball';
+import { Ball, type BallRarity } from '../entities/Ball';
+
+const BALL_RARITIES: readonly BallRarity[] = [
+  'common',
+  'rare',
+  'epic',
+  'legendary',
+  'mythical',
+];
 
 export class SpawnSystem {
   spawnBall(): Ball {
-    return new Ball(crypto.randomUUID(), 400, 300, 10, 180, 0);
+    const rarity =
+      BALL_RARITIES[Math.floor(Math.random() * BALL_RARITIES.length)];
+
+    return new Ball(crypto.randomUUID(), 400, 300, 10, 180, 0, rarity);
   }
 }
